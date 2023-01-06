f8f94369553aa4252bd84c97d1c6d75eb4a13037
jiaolong1988 <jiaolong1234567@126.com>
2023-01-06 17:09:24 +0800

    日常提交

--- 2-Java/2-基本类型使用与转换/UnderscoreTest.java
+++ 2-Java/2-基本类型使用与转换/UnderscoreTest.java
@@ -1,17 +1,19 @@
 package test.base;
-
-
-
+/**
+	下划线定义数字
+**/
 public class UnderscoreTest
 {
 	public static void main(String[] args)
 	{
-		// 定义一个32位的二进制数,最高位是符号位。
+		// 定义一个32位的二进制数,最高位是符号位。数字为：-2147483645
 		int binVal = 0B1000_0000_0000_0000_0000_0000_0000_0011;
-		double pi = 3.14_15_92_65_36;
 		System.out.println(binVal);
-		System.out.println(pi);
+		
+		//double类型下划线表示数字
+		double pi = 3.14_15_92_65_36;			
 		double height = 8_8_4_8.23;
+		System.out.println(pi);
 		System.out.println(height);
 	}
 }
